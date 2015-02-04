7472adacdabec32e90412eb790feb04352f550b8
Chocobo1 <Chocobo1@users.noreply.github.com>
2015-02-04 23:57:16 +0800

    import

---
+++ src/foo_test.cpp
@@ -0,0 +1,10 @@
+// foo_test.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+
+DECLARE_COMPONENT_VERSION(
+"foo_test",
+"0.1",
+"Sample foobar2000 component.\n"
+);
