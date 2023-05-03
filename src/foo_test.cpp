aec1e41a37ea8bbb1b82534b0cc8ddbc54f96187
Chocobo1 <Chocobo1@users.noreply.github.com>
2023-05-03 21:44:41 +0800

    Validate dll name

--- src/foo_test.cpp
+++ src/foo_test.cpp
@@ -4,7 +4,9 @@
 #include "stdafx.h"
 
 DECLARE_COMPONENT_VERSION(
-"foo_test",
-"0.1",
-"Sample foobar2000 component.\n"
+    "foo_test",
+    "0.1",
+    "Sample foobar2000 component." "\n"
 );
+
+VALIDATE_COMPONENT_FILENAME("foo_test.dll");
